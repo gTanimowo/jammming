5c5a34b6512a2393a901ae812f7038d56b24ad73
gTanimowo <goldendennar@gmail.com>
2025-07-27 00:07:47 +0100

    Fix usersPlaylist

--- src/components/UsersPlaylist/UsersPlaylist.js
+++ src/components/UsersPlaylist/UsersPlaylist.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Spotify from "../../utils/spotify";
 import styles from "../SearchResults/SearchResults.module.css";
 
